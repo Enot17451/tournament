6f23e7f092ea99aebab482e23d037b733359a6e7
Enot17451 <52169985+Enot17451@users.noreply.github.com>
2024-07-02 16:04:35 +0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,22 +1,34 @@
 pub fn sublist<T: PartialEq>(f: &[T], s: &[T]) -> Comparison {
-    let big:&[T];
-    let small:&[T];
-    if f.len()>s.len() {
-        big = f;
-        small = s;
-    }else {
-        big = s;
-        small = f;
+    if f.is_empty() && s.is_empty() {return Comparison::Equal }
+    if f.is_empty() {return Comparison::Sublist  }
+    if s.is_empty() {return Comparison::Superlist }
+    return if f.len() >= s.len() {
+        search(f, s)
+    } else {
+        let result = search(s, f);
+        match result {
+            Comparison::Sublist => Comparison::Superlist,
+            Comparison::Superlist => Comparison::Sublist,
+            _ => result
+        }
     }
+}
+
+fn search<T: PartialEq>(small: &[T], big: &[T])->Comparison{
     for a in 0..big.len() {
+        println!("small1 - {:p}",&small[0]);
+        println!("big1 - {:p}",&big[a]);
+        println!("***");
         if small[0] == big[a]{
             for b in 0..small.len() {
                 if small[b] != big[a+b] {
-                    return Comparison::Unequal
+                    println!("small2 - {:p}",&small[b]);
+                    println!("big2 - {:p}",&big[b+a]);
+                    println!("***");
+                    break;
                 }
             }
-            return Comparison::Sublist
         }
     }
-    return Comparison::Unequal
+    return Comparison::Unequal;
 }
