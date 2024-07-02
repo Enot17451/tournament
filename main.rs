fe3e7eb8f5753c9154348d8cc192879ea0a6d316
Enot17451 <52169985+Enot17451@users.noreply.github.com>
2024-07-03 01:14:49 +0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,34 +1,24 @@
 pub fn sublist<T: PartialEq>(f: &[T], s: &[T]) -> Comparison {
-    if f.is_empty() && s.is_empty() {return Comparison::Equal }
-    if f.is_empty() {return Comparison::Sublist  }
-    if s.is_empty() {return Comparison::Superlist }
-    return if f.len() >= s.len() {
-        search(f, s)
-    } else {
-        let result = search(s, f);
-        match result {
-            Comparison::Sublist => Comparison::Superlist,
-            Comparison::Superlist => Comparison::Sublist,
-            _ => result
-        }
-    }
+    if f.is_empty() && s.is_empty() { return Comparison::Equal; }
+    if f.is_empty() { return Comparison::Sublist; }
+    if s.is_empty() { return Comparison::Superlist; }
+    if f == s { return Comparison::Equal; }
+    if containSlice(f, s) { return Comparison::Superlist; }
+    if containSlice(s, f) { return Comparison::Sublist; }
+    Comparison::Unequal
 }
 
-fn search<T: PartialEq>(small: &[T], big: &[T])->Comparison{
-    for a in 0..big.len() {
-        println!("small1 - {:p}",&small[0]);
-        println!("big1 - {:p}",&big[a]);
-        println!("***");
-        if small[0] == big[a]{
-            for b in 0..small.len() {
-                if small[b] != big[a+b] {
-                    println!("small2 - {:p}",&small[b]);
-                    println!("big2 - {:p}",&big[b+a]);
-                    println!("***");
-                    break;
+pub fn containSlice<T: PartialEq>(big: &[T], small: &[T]) -> bool {
+    if big.len() < small.len() { return false; }
+    let res = false;
+    for i in 0..big.len() {
+        if big[i] == small[0] {
+            for j in 1..small.len() {
+                if big[i + j] != small[j] {
+
                 }
             }
         }
     }
-    return Comparison::Unequal;
+    true
 }
